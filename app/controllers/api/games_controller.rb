ce0361b1081013b34255525b51b6bb431b9b0a1b
halogenandtoast <halogenandtoast@gmail.com>
2016-04-27 14:22:37 -0400

    Redirect when game is destroyed

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,6 +1,10 @@
 class Api::GamesController < ApplicationController
   def show
-    game = Game.find(params[:id])
-    render json: game
+    game = Game.find_by(id:params[:id])
+    if game
+      render json: game
+    else
+      render json: { deleted: true }
+    end
   end
 end
